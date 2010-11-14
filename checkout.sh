f8a84b6d6169b5125719efd9972ec01dd2444512
Daniel Kastl <daniel@georepublic.de>
2010-11-14 18:37:13 +0900

    build and depluy script

---
+++ checkout.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+###############################################################
+# 
+# Purpose: Automated build and update of pgRouting website
+# Authors: Daniel Kastl <daniel@georepublic.de>
+#
+###############################################################
+# Copyright (c) 2010 Georepublic UG
+#
+# Licensed under the GNU LGPL.
+# 
+# This library is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 2.1 of the License,
+# or any later version.  This library is distributed in the hope that
+# it will be useful, but WITHOUT ANY WARRANTY, without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Lesser General Public License for more details, either
+# in the "LICENSE.LGPL.txt" file distributed with this software or at
+# web page "http://www.fsf.org/licenses/lgpl.html".
+#
+
+# About:
+# =====
+# This script will run the following tasks
+#       - Update to the latest changes of the website repository 
+#       - Build documentation with Sphinx
+#
+# NOTE: This script was written to run on Ubuntu server. It requires an existing
+#		clone of pgRouting website repository
+
+# Running:
+# =======
+# ./checkout.sh
+
+REPDIR="/opt/pgrouting/website/"
+WEBDIR="/var/www/pgrouting/"
+
+cd $REPDIR
+
+# Update to latest version
+git pull origin
+
+# Create HTML
+make -b html -Q -c $WEBDIR
+
+
